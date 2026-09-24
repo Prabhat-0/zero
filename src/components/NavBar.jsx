e1a4db116a7a6a2aec0a8d48a73eb9d790459184
Prabhat-0 <prabhat@joynstack.com>
2026-09-24 12:02:06 +0530

    intitialCommit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NavBar = () => {
+    const navLinks=["Home","Movies","TvShows","ContactUs"];
+    return (
+        <div>NavBar</div>
+    )
+}
+
+export default NavBar
